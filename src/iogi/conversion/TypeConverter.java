4f7cfdcf8d76d95fe24cdfa7d7459f0bab2ecef3
Rafael Ferreira <public@rafaelferreira.net>
2009-05-25 12:30:05 -0300

    TypeConverter superclass now throws meaningful exceptions.

--- src/iogi/conversion/TypeConverter.java
+++ src/iogi/conversion/TypeConverter.java
@@ -10,7 +10,16 @@ public abstract class TypeConverter<T> implements Instantiator<T> {
 	
 	@Override
 	public final T instantiate(Target<?> target, Parameters parameters) {
-		return convert(parameters.namedAfter(target).getValue(), target);
+		String stringValue = parameters.namedAfter(target).getValue();
+		try {
+			return convert(stringValue, target);
+		} 
+		catch (ConversionException e) {
+			throw e;
+		} 
+		catch (Exception e) {
+			throw new ConversionException(e, "Exception when trying to convert '%s' to a '%s' named '%s'", stringValue, target.getType(), target.getName());
+		}
 	}
 
 }
